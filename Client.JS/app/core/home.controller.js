799cbf596d2c6079faddab892f37a4ae86120f16
jogieglo@future-processing.com <jogieglo@future-processing.com>
2017-08-22 11:09:59 +0200

    Stage-1 home page refactor

--- Client.JS/app/core/home.controller.js
+++ Client.JS/app/core/home.controller.js
@@ -1,4 +1,4 @@
-﻿(function () {
+﻿(function() {
     'use strict';
 
     angular
@@ -17,38 +17,52 @@
         vm.managementValues = undefined;
         vm.recruitmentValues = undefined;
         vm.secretValues = undefined;
-        vm.authorized = true;
+        vm.recruitmentAuthorized = true;
+        vm.managementAuthorized = true;
+        vm.secretAuthorized = true;
 
         function _clearStorage() {
             localStorage['access_token'] = undefined;
         }
 
         function _getData() {
-            RegularResource.PublicValues.query({ommitBearerToken: true}, function (response) {
-                vm.publicValues = response;
-            }, function (error) {
-                console.log(error);
-            });
-
-            RegularResource.ManagementValues.query({}, function (response) {
-                vm.managementValues = response;
-                vm.authorized = true;
-            },function(error) {
-                console.log(error);
-                vm.authorized = false;
-            });
-
-            RegularResource.RecruitmentValues.query({}, function (response) {
-                vm.recruitmentValues = response;
-            }, function (error) {
-                console.log(error);
+            RegularResource.PublicValues.query({ ommitBearerToken: true },
+                function(response) {
+                    vm.publicValues = response;
+                },
+                function(error) {
+                    console.log(error);
                 });
 
-            SecretResource.SecretValues.query({}, function (response) {
-                vm.secretValues = response;
-            }, function (error) {
-                console.log(error);
-            });
+            RegularResource.ManagementValues.query({},
+                function(response) {
+                    vm.managementValues = response;
+                    vm.managementAuthorized = true;
+                },
+                function(error) {
+                    console.log(error);
+                    vm.managementAuthorized = false;
+                });
+
+            RegularResource.RecruitmentValues.query({},
+                function(response) {
+                    vm.recruitmentValues = response;
+                    vm.recruitmentAuthorized = true;
+                },
+                function(error) {
+                    console.log(error);
+                    vm.recruitmentAuthorized = false;
+                });
+
+            SecretResource.SecretValues.query({},
+                function(response) {
+                    vm.secretValues = response;
+                    vm.secretAuthorized = true;
+                },
+                function(error) {
+                    console.log(error);
+                    vm.secretAuthorized = false;
+                });
         }
 
     }
