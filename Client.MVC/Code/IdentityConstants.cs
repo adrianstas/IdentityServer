d74da3ce86ec170346403ee8d66bebeff2e225c5
jogieglo@future-processing.com <jogieglo@future-processing.com>
2017-09-06 10:18:51 +0200

    Stage-3 addition of client credentials to MVC client

--- Client.MVC/Code/IdentityConstants.cs
+++ Client.MVC/Code/IdentityConstants.cs
@@ -6,6 +6,9 @@
         {
             public const string APIRegular = "https://localhost:44321/api/values";
             public const string APISecret = "https://localhost:44310/api/values";
+
+            public const string TokenEndoint = "https://localhost:44388/connect/token";
+            public const string MVCClientSecret = "client_credential";
         }
     }
 }
--- Client.MVC/Code/OidcClient.cs
+++ Client.MVC/Code/OidcClient.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Web;
+using IdentityModel.Client;
 
 namespace Client.MVC.Code
 {
@@ -11,12 +13,38 @@ namespace Client.MVC.Code
         {
             HttpClient client = new HttpClient();
 
+            var accessToken = RequestAccessTokenClientCredentials();
+            client.SetBearerToken(accessToken);
+
             client.BaseAddress = new Uri(apiAddress);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
 
             return client;
+        }
+
+        private static string RequestAccessTokenClientCredentials()
+        {
+            var cookie = HttpContext.Current.Request.Cookies.Get("ClientMVCCookie.ClientCredentials");
+            if (cookie != null && cookie["access_token"] != null && !string.IsNullOrEmpty(cookie["access_token"]))
+            {
+                return cookie["access_token"];
+            }
+
+            var oAuth2Client = new TokenClient(
+                      IdentityConstants.TokenEndoint,
+                      "client_credentials",
+                      IdentityConstants.MVCClientSecret);
+
+            var tokenResponse = oAuth2Client.RequestClientCredentialsAsync("secret regular").Result;
+
+            var httpCookie = HttpContext.Current.Response.Cookies["ClientMVCCookie.ClientCredentials"];
+            httpCookie.Expires = DateTime.Now.AddMinutes(1);
+            if (httpCookie != null)
+                httpCookie["access_token"] = tokenResponse.AccessToken;
+
+            return tokenResponse.AccessToken;
         }
     }
 }
